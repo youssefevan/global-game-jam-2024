1ca8f11c6c42d50a877775ae1e8b25ec2e429037
Evan <eyoussef128@gmail.com>
2024-01-26 22:06:14 -0500

    hitbox, hurtbox, health, damage

--- game/scripts/fish_fighter/states/flop.gd
+++ game/scripts/fish_fighter/states/flop.gd
@@ -1,1 +1,17 @@
 extends State
+
+func enter():
+	super.enter()
+	entity.randomize_direction()
+	entity.velocity.y = -entity.flop_power
+
+func physics_update(delta):
+	super.physics_update(delta)
+	entity.apply_movement(delta)
+	entity.apply_gravity(delta)
+	
+	if entity.is_on_floor():
+		return entity.flop
+
+func exit():
+	super.exit()
