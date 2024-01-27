be5796b0b3ca214a06d0ddc1462ca8b3bac10d4b
Evan <eyoussef128@gmail.com>
2024-01-26 23:38:02 -0500

    player hurt

--- game/scripts/player_fight/player_fight.gd
+++ game/scripts/player_fight/player_fight.gd
@@ -3,6 +3,8 @@ class_name Player
 
 @onready var idle = $StateManager/Idle
 @onready var move = $StateManager/Move
+@onready var punch = $StateManager/Punch
+@onready var hurt = $StateManager/Hurt
 
 @onready var states = $StateManager
 @onready var animator = $Animator
@@ -10,14 +12,23 @@ class_name Player
 var max_health = 100
 var current_health
 
-var speed = 5000
+var speed = 10000
 var movement_input : float
+
+var damage_taken : float
+var knockback_angle : float
+var knockback_power : float
+var hitstun_frames : int
+var hitbox_dir : float
+var got_hurt : bool
 
 var gravity = 1000
 
 func _ready():
 	states.init(self)
 	current_health = max_health
+	
+	$Hitbox/Collider.disabled = true
 
 func _physics_process(delta):
 	states.physics_update(delta)
@@ -37,8 +48,16 @@ func apply_gravity(delta):
 
 func _on_hurtbox_area_entered(area):
 	if area.is_in_group("Enemy"):
-		get_hurt(area.damage)
+		get_hurt(area)
 
-func get_hurt(damage):
-	current_health -= damage
-	print(current_health, "/" , max_health)
+func get_hurt(area):
+	damage_taken = area.damage
+	hitstun_frames = area.hitstun
+	knockback_angle = area.knockback_angle
+	knockback_power = area.knockback_power
+	hitbox_dir = area.global_position.x - global_position.x
+	got_hurt = true
+
+func take_damage():
+	current_health -= damage_taken
+	print("Player: ", current_health, "/" , max_health)
