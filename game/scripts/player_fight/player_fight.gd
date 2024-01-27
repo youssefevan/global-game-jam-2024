bc63c49261802d0bb453f15f2faba32b939ea220
Evan <eyoussef128@gmail.com>
2024-01-26 20:49:50 -0500

    player fight

--- game/scripts/player_fight/player_fight.gd
+++ game/scripts/player_fight/player_fight.gd
@@ -5,8 +5,17 @@ extends CharacterBody2D
 @onready var states = $StateManager
 @onready var animator = $Animator
 
+var speed = 500
+var movement_input : float
+
 func _ready():
 	states.init(self)
 
 func _physics_process(delta):
 	states.physics_update(delta)
+
+func handle_input() -> void:
+	movement_input = Input.get_action_strength("right") - Input.get_action_strength("left")
+
+func apply_movement(delta):
+	velocity.x = movement_input * speed * delta
