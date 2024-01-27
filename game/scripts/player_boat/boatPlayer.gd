62cdd6ad95c79fdff27017506772a623548a6cd5
8by4 <aidanquinn147@gmail.com>
2024-01-26 23:39:39 -0500

    boat cast

--- game/scripts/player_boat/boatPlayer.gd
+++ game/scripts/player_boat/boatPlayer.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
 
-var speed = 300.0
+var speed = 2
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	move_and_slide()
-	
-	position += Input.get_vector("left", "right", "up", "down") * speed
+	movement()
+
+func cast():
+	if Input.is_action_just_pressed("cast"):
+		if $Rod/Line.get_point_count() >= 2:
+			self.set_process_input(false)
+		else:
+			$Rod/Line.add_point(get_local_mouse_position())
+func movement():
+	cast()
+	if $Rod/Line.get_point_count() >= 2:
+		self.set_process_input(false)
+	else:
+		position += Input.get_vector("left", "right", "up", "down") * speed
