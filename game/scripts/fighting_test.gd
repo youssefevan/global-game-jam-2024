f9595ac3140a2b1652f9ecf66dfaba0edc284cbf
Evan <eyoussef128@gmail.com>
2024-01-28 02:50:28 -0500

    finished mvp game loop

--- game/scripts/fighting_test.gd
+++ game/scripts/fighting_test.gd
@@ -3,12 +3,33 @@ extends Node2D
 @onready var fish = preload("res://scenes/fighting/fish_fighter.tscn")
 @onready var big_fish = preload("res://scenes/fighting/big_fish_fighter.tscn")
 
+var player
+var enemy
+
 func _ready():
+	var f
 	if Global.fish_type == 0:
-		var f = fish.instantiate()
+		f = fish.instantiate()
 		add_child(f)
 		f.global_position = $SpawnPosition.global_position
 	else:
-		var f = big_fish.instantiate()
+		f = big_fish.instantiate()
 		add_child(f)
 		f.global_position = $SpawnPosition.global_position
+	
+	player = $PlayerFight
+	enemy = f
+
+func _physics_process(delta):
+	if enemy.current_health <= 0 and $GUI/Win.visible == false:
+		$GUI/Win.visible = true
+	if player.current_health <= 0 and $GUI/Death.visible == false:
+		$GUI/Death.visible = true
+
+func _on_death_visibility_changed():
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_win_visibility_changed():
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://scenes/fight_screen.tscn")
