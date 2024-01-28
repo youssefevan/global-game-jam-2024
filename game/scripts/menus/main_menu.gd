34447a7f9921cfa92ed64f5621ceabbb56bc74ed
Evan <eyoussef128@gmail.com>
2024-01-27 23:55:08 -0500

    fishing stuff

--- game/scripts/menus/main_menu.gd
+++ game/scripts/menus/main_menu.gd
@@ -1,11 +1,7 @@
 extends Control
 
-
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/fighting_test.tscn")
-
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
